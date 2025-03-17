60e8734baf3eddf9428152a0bf5888e0b9c56ac5
brunobernalsalaberria <148864183+brunobernalsalaberria@users.noreply.github.com>
2025-03-17 14:05:47 +0100

    Add files via upload

---
+++ SpringBoot2dTry/src/main/java/com/digitechfp/curso_ad/springboot2dtry/SpringBoot2dTryApplication.java
@@ -0,0 +1,12 @@
+package com.digitechfp.curso_ad.springboot2dtry;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBoot2dTryApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBoot2dTryApplication.class, args);
+    }
+}
---
+++ SpringBoot2dTry/src/test/java/com/digitechfp/curso_ad/springboot2dtry/SpringBoot2dTryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.digitechfp.curso_ad.springboot2dtry;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBoot2dTryApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
